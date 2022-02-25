d5bcaaf78f7092134e983a788b83b9f37c0b9e16
spaulin.SEC <solene.paulin@orange.com>
2022-02-25 16:35:02 +0100

    welcome page ok

--- backend/apis/user_api.py
+++ backend/apis/user_api.py
@@ -2,21 +2,17 @@ from flask import Flask
 from backend import app
 from ..managers.user_manager import UserManager
 # from ..managers import user_manager
-# from flask import render_template
-# import os
-# CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-# _CURRENT_DIR = os.path.dirname(os.path.abspath(CURRENT_DIR))
-# __CURRENT_DIR = os.path.dirname(os.path.abspath(_CURRENT_DIR))
-# path = os.path.join(__CURRENT_DIR)
-
-
-# @app.route('/welcome', methods=['GET'])
-# def home():
-#     return render_template(path, name_recover = _welcome())
+from flask import render_template
+import os
 
 @app.route('/test')
 def hello():
     hello_display = UserManager.hello()
     return hello_display
+
+@app.route('/welcome', methods=['GET'])
+def welcome_page():
+    page = UserManager.welcome()
+    return render_template("welcome.html", name_recover = page)
 
         
--- backend/managers/user_manager.py
+++ backend/managers/user_manager.py
@@ -6,10 +6,12 @@ class UserManager():
     @classmethod
     def hello(self):
         return "Hello!"
-    # def welcome():
-    #     #default valeur = unknown person
-    #     #if get doesn't find key "name" in dict, it will return "unkwown person"
-    #     name = request.args.get("name", "unknown person")
-    #     #name_recover =name <=> variable = valeur 
-    #     #name_recover is reused in welcome.html
-    #     return name
+
+    @classmethod
+    def welcome(self):
+        #default valeur = unknown person
+        #if get doesn't find key "name" in dict, it will return "unkwown person"
+        name = request.args.get("name", "unknown person")
+        #name_recover =name <=> variable = valeur 
+        #name_recover is reused in welcome.html
+        return name
